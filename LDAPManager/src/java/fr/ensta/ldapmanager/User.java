24fb11accfc2e663dd11fff60525c99258efb88d
Pierre-Yves <pierre-yves.tachon@ensta-paristech.fr>
2018-01-30 12:42:22 +0100

    Merge branch 'master' of https://github.com/ghumeau/ASI334
    Ajout des classes LDAPConnector, Services et User

---
+++ LDAPManager/src/java/fr/ensta/ldapmanager/User.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensta.ldapmanager;
+
+/**
+ *
+ * @author eleve
+ */
+public class User {
+    private String lastName;
+    private String firstName;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String secureQuestion;
+    private String secureAnswer;
+    private boolean flagTwoFactorsAuth;
+    private int codeTwoFactorsAuth;
+    
+    
+}
